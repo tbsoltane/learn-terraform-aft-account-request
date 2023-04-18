3184db0973d44623e7a6019e351bcc79e46f977e
Tarek Ben Soltane <tbsoltane@gmail.com>
2023-04-18 10:19:59 +0000

    changed version again

--- terraform/modules/aft-account-request/versions.tf
+++ terraform/modules/aft-account-request/versions.tf
@@ -1,6 +1,6 @@
 terraform {
-#  required_version = ">= 0.15.0"
-  required_version = "~> 1.4.0"
+ required_version = ">= 0.15.0"
+ # required_version = "~> 1.4.1"
 
   required_providers {
     aws = {
