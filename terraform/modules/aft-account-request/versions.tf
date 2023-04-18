7d6de26339026093c8d2301f912310aadfc42b5e
Tarek Ben Soltane <tbsoltane@gmail.com>
2023-04-18 09:41:51 +0000

    added required version change

--- terraform/modules/aft-account-request/versions.tf
+++ terraform/modules/aft-account-request/versions.tf
@@ -1,10 +1,12 @@
 terraform {
-  required_version = ">= 0.15.0"
+#  required_version = ">= 0.15.0"
+  required_version = "~> 1.4.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.15"
+      #version = ">= 3.15"
+      version = "~> 4.63.0"
     }
   }
 }
